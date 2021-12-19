a1aca18bec2c6668f7963f67261046ce9baf0e0c
David Ramos <dramos1993@gmail.com>
2021-12-19 05:30:59 -0500

    Pushing the project

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Index.css';
+import App from './Containers/App.js'
+import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+
+
+ReactDOM.render(
+
+    <App />, document.getElementById('root'));
+
+reportWebVitals();
